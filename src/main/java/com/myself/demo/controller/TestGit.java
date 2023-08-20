3eb22c4c3326a8678bb6856296467ef6f1fb9d4e
Arthur <ace2661050889@gmail.com>
2023-08-20 08:40:18 +0800

    init

---
+++ src/main/java/com/myself/demo/controller/TestGit.java
@@ -0,0 +1,15 @@
+package com.myself.demo.controller;
+
+/**
+ * @Author：摘到星星了吗
+ * @Date:2023/8/20 -08-20-8:15
+ * @Description: com.myself.demo.controller
+ * @version:1.0
+ */
+public class TestGit {
+
+    public void testForMyself(){
+        String init = "首次提交";
+    }
+
+}
